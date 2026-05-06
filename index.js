f57af858d2bf0bd8b92b6a98e9cd63227d8282eb
Musiker15 <info@musiker15.de>
2026-05-06 18:55:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const { TicketClient } = require('./src/client');
+const { version } = require('./package.json');
+console.log(`[BOOT] discord-ticketbot v${version} starting...`);
 
 const client = new TicketClient();
 client.start().catch(err => {
